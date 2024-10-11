ac1579b201db12d94144a79eda4fcb22a1117612
Kateryna Pohorila <katpog2001@gmail.com>
2024-10-11 14:42:03 +0300

    add component layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SubscribeComponent } from "./subscribe/subscribe.component";
+import { MarketDataComponent } from "./market-data/market-data.component";
+import { ChartComponent } from "./chart/chart.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SubscribeComponent, MarketDataComponent, ChartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/market-data/market-data.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-market-data',
+  standalone: true,
+  imports: [],
+  templateUrl: './market-data.component.html',
+  styleUrl: './market-data.component.css'
+})
+export class MarketDataComponent {
+
+}
