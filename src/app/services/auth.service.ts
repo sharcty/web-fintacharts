77188250b88a4e3ee8480a5c9a4ddc36f353bd4f
Kateryna Pohorila <kateryna.pohorila@advantiss.com>
2024-10-23 20:16:43 +0300

    fix cors

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,26 +8,28 @@ import { map } from 'rxjs/operators';
 })
 export class AuthService {
   private apiKey: string | null = null;
-  
-  constructor(private http: HttpClient) {}
 
-  login(username: string, password: string): Observable<string| null> {
-    const url = 'https://platform.fintacharts.com/identity/realms/fintatech/protocol/openid-connect/token';
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string): Observable<string | null> {
+    const url = '/identity/realms/fintatech/protocol/openid-connect/token';
     const data = new HttpParams()
-    .set('client_id', 'app-cli')
-    .set('username', username)
-    .set('password', password)
-    .set('grant_type', 'password');
+      .set('client_id', 'app-cli')
+      .set('username', username)
+      .set('password', password)
+      .set('grant_type', 'password');
 
     return this.http.post<any>(url, data.toString(), {
-      headers: new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With'})
+      headers: new HttpHeaders({
+        'Content-Type': 'application/x-www-form-urlencoded'
       })
-    .pipe(
-      map(response => {
-        this.apiKey = response.access_token;
-        return this.apiKey;
-      })
-    );
+    })
+      .pipe(
+        map(response => {
+          this.apiKey = response.access_token;
+          return this.apiKey;
+        })
+      );
   }
 
   getApiKey(): string | null {
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -8,22 +8,20 @@ import { AuthService } from './auth.service';
 })
 export class DataService {
   private apiUrl = 'https://platform.fintacharts.com';
-  private headers:HttpHeaders;
+  private headers: HttpHeaders;
 
   constructor(private http: HttpClient, private authService: AuthService) {
     this.headers = new HttpHeaders({
       'Authorization': `Bearer ${authService.getApiKey()}`,
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With'
     });
 
   }
 
   getInstruments(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/api/instruments/v1/instruments?provider=oanda&kind=forex`, { headers: this.headers}, );
+    return this.http.get<any>(`/api/instruments/v1/instruments?provider=oanda&kind=forex`, { headers: this.headers },);
   }
 
-  getHistoricalData(id:string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/api/bars/v1/bars/count-back?instrumentId=${id}&provider=oanda&interval=1&periodicity=minute&barsCount=10`, { headers: this.headers}, );
+  getHistoricalData(id: string): Observable<any> {
+    return this.http.get<any>(`/api/bars/v1/bars/count-back?instrumentId=${id}&provider=oanda&interval=1&periodicity=minute&barsCount=10`, { headers: this.headers },);
   }
 }
